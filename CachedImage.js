68cf530da1ef64cbc71260d54adb24354d2bf050
Gaurav garg <gaurav321garg@gmail.com>
2020-06-03 11:52:53 +0530

    Update code as per new NetInfo API

--- CachedImage.js
+++ CachedImage.js
@@ -79,22 +79,22 @@ class CachedImage extends React.Component {
 
     componentWillMount() {
         this._isMounted = true;
-        NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectivityChange);
-        // initial
-        NetInfo.isConnected.fetch()
-            .then(isConnected => {
-                this.safeSetState({
-                    networkAvailable: isConnected
-                });
-            });
+
+        this.unsubscribe = NetInfo.addEventListener(this.handleConnectivityChange);
 
         this.processSource(this.props.source);
     }
+    handleConnectivityChange = state => {
+        this.safeSetState({
+            networkAvailable: state.isConnected
+        });
+    };
 
     componentWillUnmount() {
         this._isMounted = false;
-        NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectivityChange);
+        this.unsubscribe();
     }
+
 
     componentWillReceiveProps(nextProps) {
         if (!_.isEqual(this.props.source, nextProps.source)) {
